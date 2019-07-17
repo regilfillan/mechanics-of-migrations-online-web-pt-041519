4648f0b4af622b0084b7debdbb8eac932e373230
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-17 02:47:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -18,15 +18,3 @@ class CreateArtists < ActiveRecord::Migration[5.2]
 
 end
 
-
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
