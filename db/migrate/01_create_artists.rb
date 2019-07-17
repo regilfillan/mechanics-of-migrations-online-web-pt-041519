d210bc41182d8cffe172f54c812f970c7f08dda4
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-17 02:46:51 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -9,6 +9,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   
   def change
     create_table :artists do |t|
+      t.string :name 
+      t.string :genre 
+      t.integer :age 
+      t.string :hometown
+    end
   end
 
 end
